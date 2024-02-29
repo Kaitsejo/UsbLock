9fd53b93e657f4314e62fe0b6ab3ad1ec787c397
Kaitsejo <148959515+Kaitsejo@users.noreply.github.com>
2024-02-29 16:42:55 +0100

    Update LogOnUSB.ps1

--- LogOnUSB.ps1
+++ LogOnUSB.ps1
@@ -6,4 +6,4 @@ $Principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType
 
 # Créer la tâche planifiée
 $Task = New-ScheduledTask -Action $Action -Principal $Principal -Trigger $Trigger -Settings $Settings
-Register-ScheduledTask -TaskName "EnableUSBatLogon" -InputObject $Task
+Register-ScheduledTask -TaskName "EnableUSBDevicesBatLogon" -InputObject $Task
