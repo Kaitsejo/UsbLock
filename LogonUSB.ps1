871e6d5f0306e9b412761e48dabacf4bf563d67b
Kaitsejo <148959515+Kaitsejo@users.noreply.github.com>
2024-02-29 16:03:44 +0100

    Update LogonUSB.ps1

--- LogonUSB.ps1
+++ LogonUSB.ps1
@@ -1,1 +1,9 @@
+# Définir les paramètres de la tâche planifiée
+$Action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-File "C:\"'
+$Trigger = New-ScheduledTaskTrigger -AtLogon
+$Settings = New-ScheduledTaskSettingsSet
+$Principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount
 
+# Créer la tâche planifiée
+$Task = New-ScheduledTask -Action $Action -Principal $Principal -Trigger $Trigger -Settings $Settings
+Register-ScheduledTask -TaskName "DisableUSBDevices" -InputObject $Task
